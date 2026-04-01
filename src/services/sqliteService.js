47f7607c1fe0974865fd6621d9f7ec0e3378cd48
DanielDev87 <danielhymtj@icloud.com>
2026-04-01 12:55:17 -0500

    Ajustes al service de sqlite para ser testeado tambien en la web

--- src/services/sqliteService.js
+++ src/services/sqliteService.js
@@ -1,21 +1,36 @@
-import * as SQLite from 'expo-sqlite'
+import { Platform } from 'react-native'
 
-const db = SQLite.openDatabaseSync('nombre_de>-su_base_de_datos');
+let db = null
+
+if (Platform.OS !== 'web') {
+  const SQLite = require('expo-sqlite')
+  db = SQLite.openDatabaseSync('nombre_de_su_base_de_datos')
+}
 
 // Dentro de este archivo irán las sentencias y queries sql
 
-const init = ()=>{
-    //Logica para arrancar la bd
+const init = () => {
+  if (!db) {
+    console.warn('SQLite no disponible en web. Use un almacenamiento alternativo.')
+    return
+  }
+  // Logica para arrancar la bd
 }
 
-const actualizarGasolina =()=>{
-    const result = db.runSync(
-        //CONSULTA SQL PARA UPDATE
-    )
+const actualizarGasolina = () => {
+  if (!db) {
+    console.warn('SQLite no disponible en web. La actualización de gasolina no se ejecuta.')
+    return null
+  }
 
+  const result = db.runSync(
+    //CONSULTA SQL PARA UPDATE
+  )
+
+  return result
 }
 
 export default {
-    init,
-    actualizarGasolina
+  init,
+  actualizarGasolina,
 }
